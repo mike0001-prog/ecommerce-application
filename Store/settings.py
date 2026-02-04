f599df7939eba5b923c6c6ec0d1eb8f5a1cae023
oyeniyi kehinde <kehindeoyeniyi08@gmail.com>
2026-02-04 07:45:39 -0800

    untracked sqlite3

--- Store/settings.py
+++ Store/settings.py
@@ -136,7 +136,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='kehindeoyeniyi08@gmail.com'
 EMAIL_HOST_PASSWORD= os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS=True
-EMAIL_PORT = 587
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
